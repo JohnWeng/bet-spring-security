908a3ebbe3d8ea4beb35f98f376fed32617f8ca2
johnweng <hweng1953@gmail.com>
2016-07-14 16:25:13 -0400

    222

--- rest/src/main/java/demo/DemoApplication.java
+++ rest/src/main/java/demo/DemoApplication.java
@@ -2,12 +2,18 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
+import org.springframework.session.web.http.HttpSessionStrategy;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+import java.util.HashMap;
 import java.util.Map;
+
 /**
  * <OL>
  * <LI> Confirm that the Redis instance is avaialbe. Perhaps run {@code FLUSHDB}, but only </LI>
@@ -20,26 +26,26 @@ import java.util.Map;
  * <LI>Verify the results: {@code redis-cli keys '*' | xargs redis-cli del}</LI>
  * </OL>
  */
-//@EnableRedisHttpSession
+@EnableRedisHttpSession
 @SpringBootApplication
-
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-//	@Bean
-//	HttpSessionStrategy httpSessionStrategy() {
-//		return new HeaderHttpSessionStrategy();
-//	}
+	@Bean
+	HttpSessionStrategy httpSessionStrategy() {
+		return new HeaderHttpSessionStrategy();
+	}
 
-//	@RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
-//	Map<String, String> helloUser(Principal principal) {
-//		Map<String, String> result = new HashMap<>();
-//		result.put("username", principal.getName());
-//		return result;
-//	}
+	@RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+	Map<String, String> helloUser(Principal principal) {
+		Map<String, String> result = new HashMap<>();
+		result.put("usernamlllllllllle", principal.getName());
+		return result;
+	}
 //
 //	@RequestMapping("/logout")
 //	@ResponseStatus(HttpStatus.NO_CONTENT)
